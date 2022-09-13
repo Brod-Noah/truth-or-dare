3b23ad7b27235b3c973239a9617bc365a4d6b908
Brod-Noah <112982341+Brod-Noah@users.noreply.github.com>
2022-09-13 12:55:51 -0600

    added B and A buttons

--- main.ts
+++ main.ts
@@ -1,15 +1,47 @@
-input.onGesture(Gesture.Shake, function () {
-    Random_Number = randint(0, 3)
-    if (Random_Number == 0) {
-        basic.showString("Answer Unclear")
-    } else if (Random_Number == 0) {
-        basic.showString("No")
-    } else {
-        basic.showString("Yes")
+input.onButtonPressed(Button.A, function () {
+    T_or_D = randint(0, 1)
+    if (T_or_D == 0) {
+        basic.showString("Truth")
+    } else if (T_or_D == 1) {
+        basic.showString("Dare")
     }
 })
-let Random_Number = 0
-basic.showString("8 Ball")
-basic.forever(function () {
-	
+input.onButtonPressed(Button.B, function () {
+    Arrows = randint(0, 3)
+    if (Arrows == 0) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    } else if (Arrows == 1) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    } else if (Arrows == 2) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (Arrows == 3) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
 })
+let Arrows = 0
+let T_or_D = 0
+basic.showString("Truth or Dare")
